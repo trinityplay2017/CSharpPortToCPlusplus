b13ddc33519304f79b0ca95a9ece7976089f65d7
trinityplay2017 <trinityplay2017@gmail.com>
2026-09-14 07:26:43 +0800

    updated

--- RoundButtonDlg.cpp
+++ RoundButtonDlg.cpp
@@ -33,29 +33,29 @@ BOOL CRoundButtonDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
-	m_btnRound.SetRadius(18);
-	m_btnRound.SetBorderWidth(5.0f);
+	m_btnRound.SetRadius(4);
+	m_btnRound.SetBorderWidth(1.0f);
 	m_btnRound.SetWindowText(_T("Say Hi"));
 
 	// Per-state background colors
 	m_btnRound.SetStateBackgroundColor(RBS_Normal,   RGB(30, 30, 40));
-	m_btnRound.SetStateBackgroundColor(RBS_Hover,    RGB(55, 55, 80));
-	m_btnRound.SetStateBackgroundColor(RBS_Pressed,  RGB(15, 15, 22));
-	m_btnRound.SetStateBackgroundColor(RBS_Disabled, RGB(70, 70, 75));
+	m_btnRound.SetStateBackgroundColor(RBS_Hover,    RGB(55, 255, 80));
+	m_btnRound.SetStateBackgroundColor(RBS_Pressed,  RGB(255, 15, 22));
+	m_btnRound.SetStateBackgroundColor(RBS_Disabled, RGB(70, 70, 255));
 
 	// Per-state border colors (used when rainbow is off)
-	m_btnRound.SetStateBorderColor(RBS_Normal,   RGB(0, 120, 220));
-	m_btnRound.SetStateBorderColor(RBS_Hover,    RGB(80, 180, 255));
-	m_btnRound.SetStateBorderColor(RBS_Pressed,  RGB(0, 70, 140));
+	m_btnRound.SetStateBorderColor(RBS_Normal,   RGB(255, 120, 220));
+	m_btnRound.SetStateBorderColor(RBS_Hover,    RGB(100, 0, 255));
+	m_btnRound.SetStateBorderColor(RBS_Pressed,  RGB(0, 255, 0));
 	m_btnRound.SetStateBorderColor(RBS_Disabled, RGB(110, 110, 110));
 
 	// Smooth color transitions between states
 	m_btnRound.SetColorLerp(true);
-	m_btnRound.SetColorLerpDuration(180);   // ms for full transition
+	m_btnRound.SetColorLerpDuration(270);   // ms for full transition
 	m_btnRound.SetColorLerpEase(RBE_EaseOut);
 
 	// Rainbow border + animation
-	m_btnRound.SetRainbowBorder(true);
+	m_btnRound.SetRainbowBorder(false);
 	m_btnRound.SetRainbowAnimate(true);
 	m_btnRound.SetRainbowSpeed(16);         // timer interval ms
 
